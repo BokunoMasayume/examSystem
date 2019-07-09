1e294b0f5c897a9677421d1b1512cf5596c1eeb9
BokunoMasayume <BokunoMasayume@163.com>
2019-07-09 23:05:24 +0800

    finish login and show questions

--- testServer.js
+++ testServer.js
@@ -4,6 +4,52 @@ const http	= require("http"),
 	querystring = require("querystring");
 
 
+let userList = [{
+	token:12345678,
+	username:"allen",
+	password : 13579
+}];
+let examList = [];
+let questList = [];
+
+
+let q1 = {
+	id:123,
+	uppername:"例化",
+	type:"单选",
+	title : "“二十四史”编著使用的体例是()。",
+	options:[
+		{
+			content:"记纂体",
+			isAnswer:false,
+			order:1,
+		},
+		{
+			content:"编年体",
+			isAnswer:false,
+			order:2,
+		},
+		{
+			content:"小说",
+			isAnswer:false,
+			order:3,
+		},
+		{
+			content:"纪传体",
+			isAnswer:true,
+			order:4,
+		}
+	]
+};
+
+let q1Str = JSON.stringify(q1);
+for(let i=1;i<=33;i++){
+	let t = JSON.parse(q1Str);
+	t.id=i;
+	questList.push(t);
+}
+
+
 var server = http.createServer( function(request, response){
 	console.log("create server");
 });
@@ -18,7 +64,7 @@ server.on("request" , function(request , response){
 	console.log("request method" , request.method);
 
 	if(parsedUrl.pathname=='/' ){//index page
-		fs.readFile('./web_font/index.html' ,function(err,data){
+		fs.readFile('./web_font/home.html' ,function(err,data){
 			if(err){
 				response.writeHead(500,{"Content-Type":"text/plain"});
 				response.end("error");
@@ -50,13 +96,38 @@ server.on("request" , function(request , response){
 		}
 
 		//route
-		if(parsedUrl.pathname == '/login'){
+		if(parsedUrl.pathname == '/api/login'){
 			console.log(query);
 			response.end(JSON.stringify({token:1432443}));
-		}else if(parsedUrl.pathname == "/home"){
-			sendFile("./web_font/home.html" , response);
+		}else if(parsedUrl.pathname == "/login"){
+			sendFile("./web_font/index.html" , response);
 		}else if(parsedUrl.pathname=='/questions'){
 			sendFile("./web_font/questions.html" , response);
+		}else if(parsedUrl.pathname == '/exams'){
+			sendFile("./web_font/tests.html" , response) ;
+		}else if(parsedUrl.pathname == '/addQuest'){
+			sendFile("./web_font/addQuestion.html" , response);
+		}else if(parsedUrl.pathname == '/addExam'){
+			sendFile("./web_font/addTest.html" , response);
+		}
+		//api
+		else if(parsedUrl.pathname == '/api/search/questList'){
+			/*
+			*check token...
+			*/
+
+			let num = parseInt(query.ps) ;
+			let start = num*( parseInt(query.pn) -1);
+			let resData = {
+				state : "success",
+				descript : "",
+				count : questList.length,
+				questions : questList.slice(start , start + num)
+			};
+			
+			response.writeHead(200,{"Content-Type":"text/plain"});
+
+			response.end(JSON.stringify(resData));
 		}
 	}
 });
